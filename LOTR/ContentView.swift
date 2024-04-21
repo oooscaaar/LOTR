ff2470c54c846d90998490b8e65746a2de258752
Oscar M <empanaisimo@gmail.com>
2024-04-21 14:57:36 +0200

    Init Project

---
+++ LOTR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LOTR
+//
+//  Created by Oscar M. on 21/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(.background)
+                .resizable()
+        }.ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR/LOTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRApp.swift
+//  LOTR
+//
+//  Created by Oscar M. on 21/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
